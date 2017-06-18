50ee8a106d1afedf0e76d67652eaa67eff7bd76a
traster2010 <traster2010@mail.ru>
2017-06-18 11:38:22 +0300

    frontend created

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { Principal} from '../../shared';
 
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent {
+    constructor(private principal: Principal) {}
+
+    isAuthenticated() {
+        return this.principal.isAuthenticated();
+    }
+
+}
