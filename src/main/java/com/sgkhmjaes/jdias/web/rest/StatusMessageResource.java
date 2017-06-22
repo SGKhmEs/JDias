3d6fb505c444f7561512b937e0648ec50db137b5
Andrey Durovskiy <dubrovskiy0@gmail.com>
2017-06-22 21:10:27 +0300

    merge preparation

--- src/main/java/com/sgkhmjaes/jdias/web/rest/StatusMessageResource.java
+++ src/main/java/com/sgkhmjaes/jdias/web/rest/StatusMessageResource.java
@@ -2,11 +2,8 @@ package com.sgkhmjaes.jdias.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
 import com.sgkhmjaes.jdias.domain.StatusMessage;
-import com.sgkhmjaes.jdias.domain.TagFollowing;
-import com.sgkhmjaes.jdias.service.StatusMessageService;
-import com.sgkhmjaes.jdias.service.dto.PostDTO;
+import com.sgkhmjaes.jdias.service.PostService;
 import com.sgkhmjaes.jdias.service.dto.StatusMessageDTO;
-import com.sgkhmjaes.jdias.service.impl.PostDTOServiceImpl;
 import com.sgkhmjaes.jdias.service.impl.StatusMessageDTOServiceImpl;
 import com.sgkhmjaes.jdias.web.rest.util.HeaderUtil;
 import io.github.jhipster.web.util.ResponseUtil;
@@ -18,8 +15,6 @@ import java.util.List;
 import java.util.Optional;
 import java.net.URISyntaxException;
 import java.net.URI;
-import java.util.stream.StreamSupport;
-import static org.elasticsearch.index.query.QueryBuilders.*;
 
 /**
  * REST controller for managing StatusMessage.
@@ -30,88 +25,74 @@ public class StatusMessageResource {
 
     private final Logger log = LoggerFactory.getLogger(StatusMessageResource.class);
 
-    private static final String ENTITY_NAME = "statusMessage";
-//    private static final String ENTITY_NAME = "statusMessageDTOServiceImpl";
-    private final StatusMessageService statusMessageService;
+//    private static final String ENTITY_NAME = "statusMessage";
+    private static final String ENTITY_NAME = "statusMessageDTOServiceImpl";
+    private final PostService postService;
     private final StatusMessageDTOServiceImpl statusMessageDTOServiceImpl;
-    private final PostDTOServiceImpl postDTOServiceImpl;
 
-    public StatusMessageResource(StatusMessageService statusMessageService, StatusMessageDTOServiceImpl statusMessageDTOServiceImpl, PostDTOServiceImpl postDTOServiceImpl) {
-        this.statusMessageService = statusMessageService;
+    public StatusMessageResource(PostService postService, StatusMessageDTOServiceImpl statusMessageDTOServiceImpl) {
+        this.postService = postService;
         this.statusMessageDTOServiceImpl = statusMessageDTOServiceImpl;
-        this.postDTOServiceImpl = postDTOServiceImpl;
     }
 
     /**
-     * POST /status-messages : Create a new statusMessage.
+     * POST  /status-messages : Create a new statusMessage.
      *
      * @param statusMessage the statusMessage to create
-     * @return the ResponseEntity with status 201 (Created) and with body the
-     * new statusMessage, or with status 400 (Bad Request) if the statusMessage
-     * has already an ID
+     * @return the ResponseEntity with status 201 (Created) and with body the new statusMessage, or with status 400 (Bad Request) if the statusMessage has already an ID
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PostMapping("/status-messages")
     @Timed
-    public ResponseEntity<StatusMessage> createStatusMessage(@RequestBody StatusMessage statusMessage) throws URISyntaxException {
+    public void createStatusMessage(@RequestBody StatusMessage statusMessage) throws URISyntaxException {
         log.debug("REST request to save StatusMessage : {}", statusMessage);
-        if (statusMessage.getId() != null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "idexists", "A new statusMessage cannot already have an ID")).body(null);
-        }
-        StatusMessage result = statusMessageService.save(statusMessage);
-
-        return ResponseEntity.created(new URI("/api/statu-messages/" + result.getId()))
-                .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
-                .body(result);
+        postService.save(statusMessage);
     }
 
     /**
-     * PUT /status-messages : Updates an existing statusMessage.
+     * PUT  /status-messages : Updates an existing statusMessage.
      *
      * @param statusMessage the statusMessage to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated
-     * statusMessage, or with status 400 (Bad Request) if the statusMessage is
-     * not valid, or with status 500 (Internal Server Error) if the
-     * statusMessage couldnt be updated
+     * @return the ResponseEntity with status 200 (OK) and with body the updated statusMessage,
+     * or with status 400 (Bad Request) if the statusMessage is not valid,
+     * or with status 500 (Internal Server Error) if the statusMessage couldnt be updated
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PutMapping("/status-messages")
     @Timed
-    public void updateStatusMessage(@RequestBody StatusMessageDTO statusMessageDTO) throws URISyntaxException {
-        log.debug("REST request to update StatusMessage : {}", statusMessageDTO);
-        statusMessageDTOServiceImpl.save(statusMessageDTO);
+    public void updateStatusMessage(@RequestBody StatusMessage statusMessage) throws URISyntaxException {
+        log.debug("REST request to update StatusMessage : {}", statusMessage);
+        postService.save(statusMessage);
     }
 
     /**
-     * GET /status-messages : get all the statusMessages.
+     * GET  /status-messages : get all the statusMessages.
      *
-     * @return the ResponseEntity with status 200 (OK) and the list of
-     * statusMessages in body
+     * @return the ResponseEntity with status 200 (OK) and the list of statusMessages in body
      */
     @GetMapping("/status-messages")
     @Timed
     public List<StatusMessage> getAllStatusMessages() {
         log.debug("REST request to get all StatusMessages");
-        return statusMessageService.findAll();
+        return postService.findAllStatusMessage();
     }
 
     /**
-     * GET /status-messages/:id : get the "id" statusMessage.
+     * GET  /status-messages/:id : get the "id" statusMessage.
      *
      * @param id the id of the statusMessage to retrieve
-     * @return the ResponseEntity with status 200 (OK) and with body the
-     * statusMessage, or with status 404 (Not Found)
+     * @return the ResponseEntity with status 200 (OK) and with body the statusMessage, or with status 404 (Not Found)
      */
     @GetMapping("/status-messages/{id}")
     @Timed
     public ResponseEntity<StatusMessage> getStatusMessage(@PathVariable Long id) {
         log.debug("REST request to get StatusMessage : {}", id);
-        StatusMessage statusMessage = statusMessageService.findOne(id);
+        StatusMessage statusMessage = postService.findOneStatusMessage(id);
         return ResponseUtil.wrapOrNotFound(Optional.ofNullable(statusMessage));
     }
 
     /**
-     * DELETE /status-messages/:id : delete the "id" statusMessage.
+     * DELETE  /status-messages/:id : delete the "id" statusMessage.
      *
      * @param id the id of the statusMessage to delete
      * @return the ResponseEntity with status 200 (OK)
@@ -120,13 +101,13 @@ public class StatusMessageResource {
     @Timed
     public ResponseEntity<Void> deleteStatusMessage(@PathVariable Long id) {
         log.debug("REST request to delete StatusMessage : {}", id);
-        statusMessageService.delete(id);
+        postService.delete(id);
         return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
     }
 
     /**
-     * SEARCH /_search/status-messages?query=:query : search for the
-     * statusMessage corresponding to the query.
+     * SEARCH  /_search/status-messages?query=:query : search for the statusMessage corresponding
+     * to the query.
      *
      * @param query the query of the statusMessage search
      * @return the result of the search
@@ -135,7 +116,7 @@ public class StatusMessageResource {
     @Timed
     public List<StatusMessage> searchStatusMessages(@RequestParam String query) {
         log.debug("REST request to search StatusMessages for query {}", query);
-        return statusMessageService.search(query);
+        return postService.searchStatusMessage(query);
     }
 
 }
